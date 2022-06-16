c375ae4386e1af3fcf5266370acc162564727bb9
Kyle <kyle@kyle-personal-Air.local>
2022-06-16 19:04:48 +0800

    c

--- plugins/plugins.js
+++ plugins/plugins.js
@@ -14,17 +14,32 @@ const proxiedSend = async function (body) {
 
             // 2. call profile api to get encrypted password
             let baseUrl = window.location.protocol + '//' + window.location.host;
-            baseUrl = baseUrl + '/artifactory/api/security/encryptedPassword';
+            baseUrl = baseUrl + '/artifactory/api/security/token';
 
             let headers = new Headers();
             headers.append('Authorization', 'Basic ' + btoa(json.user + ":" + json.password));
+            headers.append('Content-Type', 'application/x-www-form-urlencoded');
 
-            const data = await fetch(baseUrl, {method:'GET', headers: headers}).then(res => res.text());
-            console.log('encrypted password=', data);
+            var details = {
+                'username': json.user,
+                'scope': 'member-of-groups:*'
+            };
+            
+            var formBody = [];
+            for (var property in details) {
+              var encodedKey = encodeURIComponent(property);
+              var encodedValue = encodeURIComponent(details[property]);
+              formBody.push(encodedKey + "=" + encodedValue);
+            }
+            formBody = formBody.join("&");
+            
+            const data = await fetch(baseUrl, {method:'POST', headers: headers, body: formBody}).then(res => res.json());
+            let token = data.access_token;
+            console.log('token=', token);
 
             // 3. for later usage in ajax header
             document.cookie = "user="+ json.user +"; max-age=3600; path=/;";
-            document.cookie = "password="+ data +"; max-age=3600; path=/;";
+            document.cookie = "token="+ token +"; max-age=3600; path=/;";
         }
     }  
     nativeSend.apply(this, arguments);
